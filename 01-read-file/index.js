0ea0867bdba1d03cbee067fa027fa271b2d8cc2b
Leta Sonce <letasonce@gmail.com>
2024-01-20 15:21:27 +0300

    fix: add solution for task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,18 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const EventEmitter = require('events');
-const eventEmitter = new EventEmitter();
 const pathFile = path.join(__dirname, 'text.txt');
 const readableStream = fs.createReadStream(pathFile, 'utf-8');
 
-eventEmitter.on('begin', () => {
-  console.log('Start reading file...');
-  readableStream.on('data', (chunk) => console.log(chunk));
-});
-
-eventEmitter.on('end', () => {
-  console.log('Finish reading file.');
-});
-
-eventEmitter.emit('begin');
-eventEmitter.emit('end');
+readableStream.on('data', (chunk) => console.log(chunk));
