7f074761b99845a97629c7fe0f85cc08b5412454
Leta Sonce <letasonce@gmail.com>
2024-01-24 17:48:06 +0300

    fix: add command exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,12 +5,17 @@ const { stdin, stdout, exit } = process;
 const pathFile = path.join(__dirname, 'text.txt');
 const newFile = fs.createWriteStream(pathFile);
 
-stdout.write('Enter data:\n');
+stdout.write('Hello student Rolling Scope!\nWrite your text and press "Enter" Write "exit" or press "ctrl + c" to stop:\n');
 stdin.on('data', (data) => {
-  newFile.write(data);
+  let str = data.toString();
+  str === 'exit\n' 
+  ? terminate()
+  : newFile.write(str);
 });
 
-process.on('SIGINT', () => {
-  stdout.write('\n\nGood Luck in JavaScript!!!\n\n');
-  exit();
-});
+process.on('SIGINT', () => terminate());
+
+function terminate() {
+  stdout.write('\nBye! Good luck!');
+  process.exit();
+}
